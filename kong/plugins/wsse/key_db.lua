edae527e2c1aab6db91bd53486afc5170203b54a
Gabor Nemeth <negabor@gmail.com>
2018-03-01 11:12:10 +0100

    add secret repository, and add classic lua for object orient inheritance implementation

---
+++ kong/plugins/wsse/key_db.lua
@@ -0,0 +1,17 @@
+local Object = require("kong.plugins.wsse.classic")
+
+local KeyDb = Object:extend()
+
+function KeyDb:find_by_username(username)
+    if username == nil then
+        error("Username is required!")
+    end
+
+    if username == "test" then
+        return "secret"
+    else
+        error("Username could not be found!")
+    end
+end
+
+return KeyDb
