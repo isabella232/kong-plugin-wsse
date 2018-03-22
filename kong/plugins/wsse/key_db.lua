75ea49166fbe44acc2c8f1772f7e520338709879
bencso <gabor.bencsik13@gmail.com>
2018-03-22 14:59:14 +0100

    add caching to wsse_key retrieval

--- kong/plugins/wsse/key_db.lua
+++ kong/plugins/wsse/key_db.lua
@@ -4,24 +4,34 @@ local Logger = require "kong.plugins.wsse.logger"
 
 local KeyDb = Object:extend()
 
+local function load_credential(username)
+    local rows, err = singletons.dao.wsse_keys:find_all {key = username}
+    if err then
+        return nil, err
+    end
+    return rows[1]
+end
+
 function KeyDb.find_by_username(username)
     if username == nil then
         Logger.getInstance(ngx):logWarning({msg = "Username is required."})
         error({msg = "Username is required."})
     end
 
-    local rows, err = singletons.dao.wsse_keys:find_all {key = username}
+    local wsse_cache_key = singletons.dao.wsse_keys:cache_key(username)
+    local wsse_key, err = singletons.cache:get(wsse_cache_key, nil, load_credential, username)
+
     if err then
         Logger.getInstance(ngx):logError(err)
         error({msg = "WSSE key can not be found."})
     end
 
-    if rows[1] == nil then
+    if wsse_key == nil then
         Logger.getInstance(ngx):logWarning({msg = "WSSE key can not be found."})
         error({msg = "WSSE key can not be found."})
     end
 
-    return rows[1]
+    return wsse_key
 end
 
 return KeyDb
