0e4350e030f3b0a920a19d23c45e5763407ae4d5
Gabor Nemeth <negabor@gmail.com>
2018-03-23 12:22:59 +0100

    change plugin default config (anonymous user string instead of table)

--- kong/plugins/wsse/schema.lua
+++ kong/plugins/wsse/schema.lua
@@ -1,7 +1,17 @@
+local utils = require "kong.tools.utils"
+
+local function check_user(anonymous)
+  if anonymous == nil or utils.is_valid_uuid(anonymous) then
+    return true
+  end
+
+  return false, "the anonymous user must be nil or a valid uuid"
+end
+
 return {
   no_consumer = true,
   fields = {
-    anonymous = { type = "table", default = nil },
+    anonymous = {type = "string", default = nil, func = check_user},
     timeframe_validation_treshhold_in_minutes = { type = "number", default = 5 }
   }
 }
