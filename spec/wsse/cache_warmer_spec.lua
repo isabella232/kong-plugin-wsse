47f3be06e96a7579d3e8278532588092280a36c7
Marton Papp <marton.papp@emarsys.com>
2019-02-08 10:29:42 +0100

    CDPIP-1049: add context to cache warmer tests

--- spec/wsse/cache_warmer_spec.lua
+++ spec/wsse/cache_warmer_spec.lua
@@ -17,41 +17,43 @@ describe("CacheWarmer", function()
         helpers.stop_kong()
     end)
 
-    it('should store consumer in cache', function()
-        helpers.start_kong({ plugins = 'wsse' })
+    context('cache_all_entities', function()
+        it('should store consumer in cache', function()
+            helpers.start_kong({ plugins = 'wsse' })
 
-        local cache_key = helpers.db.daos.consumers:cache_key(consumer.id)
+            local cache_key = helpers.db.daos.consumers:cache_key(consumer.id)
 
-        local raw_response = assert(helpers.admin_client():send {
-            method = "GET",
-            path = "/cache/" .. cache_key,
-        })
+            local raw_response = assert(helpers.admin_client():send {
+                method = "GET",
+                path = "/cache/" .. cache_key,
+            })
 
-        local body = assert.res_status(200, raw_response)
-        local response = cjson.decode(body)
+            local body = assert.res_status(200, raw_response)
+            local response = cjson.decode(body)
 
-        assert.is_equal(response.username, 'CacheTestUser')
-    end)
+            assert.is_equal(response.username, 'CacheTestUser')
+        end)
 
-    it('should store wsse_key in cache', function()
-        local wsse_credential = helpers.dao.wsse_keys:insert({
-            key = 'cache_test_user001',
-            key_lower = 'cache_test_user001',
-            consumer_id = consumer.id
-        })
+        it('should store wsse_key in cache', function()
+            local wsse_credential = helpers.dao.wsse_keys:insert({
+                key = 'cache_test_user001',
+                key_lower = 'cache_test_user001',
+                consumer_id = consumer.id
+            })
 
-        helpers.start_kong({ plugins = 'wsse' })
+            helpers.start_kong({ plugins = 'wsse' })
 
-        local cache_key = helpers.dao.wsse_keys:cache_key(wsse_credential.key)
+            local cache_key = helpers.dao.wsse_keys:cache_key(wsse_credential.key)
 
-        local raw_response = assert(helpers.admin_client():send {
-            method = "GET",
-            path = "/cache/" .. cache_key,
-        })
+            local raw_response = assert(helpers.admin_client():send {
+                method = "GET",
+                path = "/cache/" .. cache_key,
+            })
 
-        local body = assert.res_status(200, raw_response)
-        local response = cjson.decode(body)
+            local body = assert.res_status(200, raw_response)
+            local response = cjson.decode(body)
 
-        assert.is_equal(response.key, 'cache_test_user001')
+            assert.is_equal(response.key, 'cache_test_user001')
+        end)
     end)
 end)
